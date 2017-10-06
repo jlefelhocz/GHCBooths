7ab4962c6146db1244b996d99902d8a142463070
Julianne Lefelhocz <juliannelefelhocz@Juliannes-MacBook-Pro.local>
2017-10-06 15:28:45 -0400

    Initial Commit

---
+++ GHCBooths/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GHCBooths
+//
+//  Created by Julianne Lefelhocz on 10/6/17.
+//  Copyright © 2017 Julianne Lefelhocz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
